973e3ac5d3fb98d2c6fd8637f6c01b4ac2c2b732
Ray Tran <ray@artran.co.uk>
2012-02-04 15:34:54 +0000

    Added Unit Testing bundle

---
+++ RunCalcTests/RunCalcTests.h
@@ -0,0 +1,13 @@
+//
+//  RunCalcTests.h
+//  RunCalcTests
+//
+//  Created by Ray Tran on 04/02/2012.
+//  Copyright (c) 2012 Artran Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RunCalcTests : SenTestCase
+
+@end
---
+++ RunCalcTests/RunCalcTests.m
@@ -0,0 +1,32 @@
+//
+//  RunCalcTests.m
+//  RunCalcTests
+//
+//  Created by Ray Tran on 04/02/2012.
+//  Copyright (c) 2012 Artran Ltd. All rights reserved.
+//
+
+#import "RunCalcTests.h"
+
+@implementation RunCalcTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RunCalcTests");
+}
+
+@end
