7b28116b273f44ecd7c9272c7c9cafdf9f17ea84
Ray Tran <ray@artran.co.uk>
2012-03-01 22:41:47 +0000

    Started to write tests for heart rate calculations.

---
+++ RunCalcTests/HeartRateTests.h
@@ -0,0 +1,13 @@
+//
+//  HeartRateTests.h
+//  HeartRateTests
+//
+//  Created by Ray Tran on 01/03/2012.
+//  Copyright (c) 2012 Artran Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HeartRateTests : SenTestCase
+
+@end
---
+++ RunCalcTests/HeartRateTests.m
@@ -0,0 +1,33 @@
+//
+//  HeartRateTests.m
+//  HeartRateTests
+//
+//  Created by Ray Tran on 01/03/2012.
+//  Copyright (c) 2012 Artran Ltd. All rights reserved.
+//
+
+#import "HeartRateTests.h"
+#import "HeartRateModel.h"
+
+@implementation HeartRateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testOneHundredPerCentIsMaxHr
+{
+    STAssertTrue(YES, @"");
+}
+
+@end
